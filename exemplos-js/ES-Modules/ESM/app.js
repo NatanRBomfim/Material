afe985dfb684987254bfd1389ca44a32bebdd454
Natan Bomfim <natan.bonfim6548@outlook.com>
2025-12-04 16:17:50 -0300

    Corrigdo os nomes de arquivos no from.

--- exemplos-js/ES-Modules/ESM/app.js
+++ exemplos-js/ES-Modules/ESM/app.js
@@ -1,10 +1,10 @@
-import somar, { testarNome as testarNomeSoma } from "./module/soma.js";
+import somar, { testarNome as testarNomeSoma } from "./modulejs/soma.js";
 import {
   media,
   teste,
   IP,
   testarNome as testarNomeMedia,
-} from "./module/media.js";
+} from "./modulejs/media.js";
 
 console.log(somar(1, 2, 3, 4, 5));
 console.log(media(1, 2, 3, 4, 5));
